fe1ddb3e833ae5384f551c8f1047e7940867a8f6
cch137 <96408836+cch137@users.noreply.github.com>
2023-08-22 12:59:52 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import type { TextBasedChannel } from 'discord.js'
+import { Client, IntentsBitField } from 'discord.js'
+
+const client = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+    IntentsBitField.Flags.GuildMessageReactions,
+  ]
+})
