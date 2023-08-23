d4a90532b46e479d0f18f98474059422548ed4a8
cch137 <96408836+cch137@users.noreply.github.com>
2023-08-23 17:33:25 +0800

    updated

--- src/index.ts
+++ src/index.ts
@@ -100,7 +100,7 @@ class TeacherBot {
       channel.sendTyping()
       let typingInterval = setInterval(() => channel.sendTyping(), 3000)
       const t0 = Date.now()
-      const context = dcMessagesToContext(await getRecentChannelMessages(guild, channel), clientId, 4000 - countTokensLength(this.prompt))
+      const context = dcMessagesToContext(await getRecentChannelMessages(guild, channel), clientId, 4500 - countTokensLength(this.prompt))
       console.log(`${this.name} Prepare in ${Date.now() - t0}ms`)
       try {
         const t0 = Date.now()
