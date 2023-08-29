e2c5b7c04ff9a77f5dd6a6305bf78922dfca5876
cch137 <96408836+cch137@users.noreply.github.com>
2023-08-30 03:21:11 +0800

    fx: better prompt

--- dist/bots/brain1-vs-brain2.js
+++ dist/bots/brain1-vs-brain2.js
@@ -6,7 +6,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const constants_1 = require("../constants");
 const AiBotChatroom_1 = __importDefault(require("./drivers/set/AiBotChatroom"));
 const ChannelTextBot_1 = __importDefault(require("./drivers/ChannelTextBot"));
-const brainPrompt = (name) => `你是 ${name} 。請嘗試挑起一些涉及專業知識的話題，例如數學、物理、化學、生物、哲學、文化、資訊科技等。如果當前話題已經討論完，請開啟新的話題。在回答時僅需專注於一個話題，請持續地讓對話充實。請只討論學術知識相關的話題，不討論學習經驗和聊天。請在對話中省略敬語和致謝。請對以下對話回复一條訊息。`;
+const brainPrompt = (name) => `你是 ${name} 。
+請嘗試挑起一些涉及專業知識的話題，例如數學、物理、化學、生物、哲學、文化、資訊科技等。
+在回答時僅需專注於一個話題，請持續地讓對話充實。
+請只討論學術知識相關的話題，不要討論學習建議、學習技巧之類的話題。
+如果當前話題沒有值得討論的地方，請大膽地主動開啟新話題，避免在同一個話題停留太久。
+請在對話中省略敬語和致謝。
+請對以下對話回复一條訊息。`;
 const Brain1 = new ChannelTextBot_1.default({
     token: constants_1.BRAIN1_TOKEN,
     guildId: constants_1.GUILD_ID,
