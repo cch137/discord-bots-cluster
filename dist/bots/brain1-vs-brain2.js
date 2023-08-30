05f80405b983bae790a078817383155d35c0ca5a
cch137 <96408836+cch137@users.noreply.github.com>
2023-08-30 11:23:35 +0800

    fix: better prompt

--- dist/bots/brain1-vs-brain2.js
+++ dist/bots/brain1-vs-brain2.js
@@ -7,10 +7,11 @@ const constants_1 = require("../constants");
 const AiBotChatroom_1 = __importDefault(require("./drivers/set/AiBotChatroom"));
 const ChannelTextBot_1 = __importDefault(require("./drivers/ChannelTextBot"));
 const brainPrompt = (name) => `你是 ${name} 。
-請嘗試挑起一些涉及專業知識的話題，例如數學、物理、化學、生物、哲學、文化、資訊科技等。
-在回答時僅需專注於一個話題，請持續地讓對話充實。
+請討論一些專業知識的話題，例如微積分、工程數學、電子學、電路學、電磁學、通訊技術、電腦科學等。
+在回答時請詳細和深入地講解該領域的一個知識點。
+例如，你可以深入地講述某個理論或技術，並給出應用例子。
+請不斷地開啟新話題，不要在同一個話題停留太久。
 請只討論學術知識相關的話題，不要討論學習建議、學習技巧之類的話題。
-如果當前話題沒有值得討論的地方，請大膽地主動開啟新話題，避免在同一個話題停留太久。
 請在對話中省略敬語和致謝。
 請對以下對話回复一條訊息。`;
 const Brain1 = new ChannelTextBot_1.default({
